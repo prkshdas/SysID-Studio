53c3f62fd347b9abda265ee07f4df3b5f7b654ba
Prakash <prkshhhdas@gmail.com>
2026-03-29 23:11:55 +0530

    Updated canvas_view.py with canvas scene functionalities

--- gui/canvas/canvas_view.py
+++ gui/canvas/canvas_view.py
@@ -1,8 +1,14 @@
 from PyQt5.QtWidgets import QGraphicsView
-
+from .canvas_scene import CanvasScene
 
 class CanvasView(QGraphicsView):
     def __init__(self):
         super().__init__()
-
+        
+        self.scene = CanvasScene()
+        self.setScene(self.scene)
+        
+        self.setRenderHints(self.renderHints())
         self.setAcceptDrops(True)
+        
+        self.setDragMode(QGraphicsView.rubberBandDrag)
