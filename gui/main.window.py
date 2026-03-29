2306aa018824de4ed4f9da082329e2a2185b892a
Prakash <prkshhhdas@gmail.com>
2026-03-29 22:26:32 +0530

    main window added

---
+++ gui/main.window.py
@@ -0,0 +1,21 @@
+from PyQt5.QtWidgets import QMainWindow, QWidget, QHBoxLayout
+from .canvas.canvas_view import CanvasView
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        
+        self.setWindowTitle("SysID Studio - Canvas")
+        
+        self.canvas = CanvasView()
+        
+        layout = QHBoxLayout()
+        layout.addWidget(self.canvas)
+        
+        container = QWidget()
+        container.setLayout(layout)
+        
+        self.setCentralWidget(container)
+            
+            
